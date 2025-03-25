c49c6389dd43e92a439b2aab5ec33e8fa394782f
Chinmay Pramod Bharti <48250904+Chhinna@users.noreply.github.com>
2025-03-25 11:58:55 +0530

    lowercase

--- module.cpp
+++ module.cpp
@@ -73,5 +73,13 @@ int main() {
     // Violation: Use of atoi is forbidden.
     int num = atoi("123");
 
+    // lowercase suffixes
+    const int        a = 0u;      // Noncompliant
+    const int        b = 0l;      // Noncompliant
+    const int        c = 0Ul;     // Noncompliant
+    const int        d = 0x12bu;  // Noncompliant
+    const float      m = 1.2f;    // Noncompliant
+    const float      n = 1.2l;    // Noncompliant
+
     return 0;
 }
